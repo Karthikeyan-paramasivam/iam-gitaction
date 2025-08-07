dc5834658c946ccc679f0984e06c95e2b34e3a57
Karthikeyan <karthikeyan@pippintitle.com>
2025-08-07 11:20:06 +0530

    56

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const server = http.createServer((req, res) => {
     fs.readFile(filePath, (err, content) => {
       if (err) {
         res.writeHead(500);
-        res.end('Error loading page');
+        res.end('Error loading page pippin');
       } else {
         res.writeHead(200, { 'Content-Type': 'text/html' });
         res.end(content);
